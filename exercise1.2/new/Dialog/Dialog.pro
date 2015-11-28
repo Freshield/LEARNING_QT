dc691b29ba8b0d7091d6999b6615eace96874b0c
Freshield <zxdsw199182@gmail.com>
2015-11-28 14:13:52 -0500

    using code only model

---
+++ exercise1.2/new/Dialog/Dialog.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-11-28T14:12:16
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Dialog
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+HEADERS  += dialog.h
---
+++ exercise1.2/new/Dialog/dialog.h
@@ -0,0 +1,22 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+
+#include <QDialog>
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = 0);
+    ~Dialog();
+private:
+        QLabel *label1, *label2;
+        QLineEdit *lineEdit;
+        QPushButton *button;
+};
+
+#endif // DIALOG_H
