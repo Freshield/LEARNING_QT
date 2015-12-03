2ca0406209c36ab68fb4a65871f747d8fabe4950
Freshield <zxdsw199182@gmail.com>
2015-12-03 15:13:41 -0500

    try the messagebox and close widget

---
+++ QT4.7/TEST/messagebox_test/messagebox_test/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::information(this, tr("Listen"), tr("Listen successed."));
+    return;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    close();
+}
---
+++ QT4.7/TEST/messagebox_test/messagebox_test/messagebox_test.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-12-03T15:07:57
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = messagebox_test
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
