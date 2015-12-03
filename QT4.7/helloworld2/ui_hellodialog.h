0b16e36b1dcea2244ea42ecb173f9915a0b1321f
Freshield <zxdsw199182@gmail.com>
2015-12-02 21:48:22 -0500

    done code make ui

---
+++ QT4.7/helloworld2/ui_hellodialog.h
@@ -0,0 +1,56 @@
+/********************************************************************************
+** Form generated from reading UI file 'hellodialog.ui'
+**
+** Created: Wed Dec 2 21:47:12 2015
+**      by: Qt User Interface Compiler version 4.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_HELLODIALOG_H
+#define UI_HELLODIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QDialog>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_HelloDialog
+{
+public:
+    QLabel *label;
+
+    void setupUi(QDialog *HelloDialog)
+    {
+        if (HelloDialog->objectName().isEmpty())
+            HelloDialog->setObjectName(QString::fromUtf8("HelloDialog"));
+        HelloDialog->resize(865, 607);
+        label = new QLabel(HelloDialog);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(120, 120, 380, 211));
+
+        retranslateUi(HelloDialog);
+
+        QMetaObject::connectSlotsByName(HelloDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *HelloDialog)
+    {
+        HelloDialog->setWindowTitle(QApplication::translate("HelloDialog", "Dialog", 0, QApplication::UnicodeUTF8));
+        label->setText(QApplication::translate("HelloDialog", "Hello World!!!\344\275\240\345\245\275~~~", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class HelloDialog: public Ui_HelloDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_HELLODIALOG_H
