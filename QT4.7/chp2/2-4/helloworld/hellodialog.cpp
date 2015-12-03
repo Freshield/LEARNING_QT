98e4a68cc8e15e548cf7df6f3002fdcaa5eebf08
Freshield <zxdsw199182@gmail.com>
2015-12-02 22:10:52 -0500

    done using defined cpp

---
+++ QT4.7/chp2/2-4/helloworld/hellodialog.cpp
@@ -0,0 +1,9 @@
+#include "hellodialog.h"
+#include "ui_hellodialog.h"
+
+HelloDialog::HelloDialog(QWidget *parent) :
+    QDialog(parent)
+{
+    ui = new Ui::HelloDialog;
+    ui->setupUi(this);
+}
